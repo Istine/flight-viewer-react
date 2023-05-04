3832eddabd9a0f2fc38fa3c5033ac49fce7158f6
istine <imanchesebastian@gmail.com>
2023-05-04 05:28:14 +0100

    initialized app

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/Flair_Airlines_logo.svg";
+
+const Login: React.FC<{}> = () => {
+  return (
+    <div className="container">
+      <h3>Hello Again!</h3>
+      <img src={logo} alt="flair airlines logo" />
+    </div>
+  );
+};
+
+export default Login;
