5260c175a0fd0af1887ca03a7d3484d66b06cf35
istine <imanchesebastian@gmail.com>
2023-05-04 09:24:33 +0100

    completed login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,11 +4,13 @@ import TextField from "../components/TextField";
 import { GoMention, GoEyeClosed, GoEye } from "react-icons/go";
 import Button from "../components/Button";
 import CheckBox from "../components/CheckBox";
+import { validateLogin, waitFor } from "../utils";
 
 type UserLogin = {
   email: string;
   password: string;
   check: boolean;
+  showPassword: boolean;
 };
 
 const Login: React.FC<{}> = () => {
@@ -16,7 +18,14 @@ const Login: React.FC<{}> = () => {
     email: "",
     password: "",
     check: false,
+    showPassword: false,
   });
+
+  const [loginErrorState, setLoginErrorState] = React.useState("");
+
+  const [isLoading, setLoading] = React.useState(false);
+
+  const buttonText = isLoading ? "logging in..." : "login";
 
   const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -33,69 +42,113 @@ const Login: React.FC<{}> = () => {
     }));
   };
 
-  const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const togglePasswordVisibilty = (e: React.MouseEvent<HTMLOrSVGElement>) => {
+    setLoginFormState((prevState: UserLogin) => ({
+      ...prevState,
+      showPassword: !prevState.showPassword,
+    }));
+  };
+
+  const onSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const { email, password } = loginFormState;
+    const [isValid, error] = validateLogin({ email, password });
+    if (!isValid) {
+      setLoginErrorState(error as string);
+      return;
+    } else {
+      setLoading(true);
+      await waitFor(1);
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className="wrapper">
-      <div className="container">
-        <img src={logo} alt="flair airlines logo" />
-        <h3>Hello Again!</h3>
-        <p>
-          Welcome aboard! Please enter your login details to access the latest
-          flight data.
-        </p>
-        <form>
-          <TextField
-            type="email"
-            name="email"
-            id="email"
-            handler={handleFormChange}
-            value={loginFormState.email}
-            placeholder="mike@flysafe.com"
-            required={true}
-            InputAdornment={<GoMention color="#322a43" />}
-          />
-          <TextField
-            type="password"
-            name="password"
-            id="password"
-            required={true}
-            handler={handleFormChange}
-            value={loginFormState.password}
-            placeholder="password"
-            style={{ margin: 0, marginBottom: "5px" }}
-            InputAdornment={<GoEye className="go-eye-icon" color="#322a43" />}
-          />
-          <div
-            className="form-control"
-            style={{
-              backgroundColor: "white",
-              height: "auto",
-              border: "none",
-              margin: 0,
-              marginBottom: "1rem",
-            }}
-          >
+    <>
+      <div className="wrapper">
+        <div className="container">
+          <img src={logo} alt="flair airlines logo" />
+          <h3>Hello Again!</h3>
+          <p>
+            Welcome aboard! Please enter your login details to access the latest
+            flight data.
+          </p>
+          <span className="login-form-error">{loginErrorState}</span>
+
+          <form>
+            <TextField
+              type="email"
+              name="email"
+              id="email"
+              handler={handleFormChange}
+              value={loginFormState.email}
+              placeholder="mike@flysafe.com"
+              required={true}
+              InputAdornment={<GoMention color="#322a43" />}
+            />
+            <TextField
+              type={loginFormState.showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              required={true}
+              handler={handleFormChange}
+              value={loginFormState.password}
+              placeholder="password"
+              style={{ margin: 0, marginBottom: "5px" }}
+              InputAdornment={
+                loginFormState.showPassword ? (
+                  <GoEye
+                    onClick={togglePasswordVisibilty}
+                    className="go-eye-icon"
+                    color="#322a43"
+                  />
+                ) : (
+                  <GoEyeClosed
+                    onClick={togglePasswordVisibilty}
+                    className="go-eye-icon"
+                    color="#322a43"
+                  />
+                )
+              }
+            />
             <div
               className="form-control"
-              onClick={handleCheckbox}
               style={{
-                width: "95px",
-                border: "none",
-                height: 20,
-                overflow: "hidden",
                 backgroundColor: "white",
+                height: "auto",
+                border: "none",
                 margin: 0,
+                marginBottom: "1rem",
               }}
             >
-              <CheckBox text="remember me" value={loginFormState.check} />
+              <div
+                className="form-control"
+                onClick={handleCheckbox}
+                style={{
+                  width: "95px",
+                  border: "none",
+                  height: 20,
+                  overflow: "hidden",
+                  backgroundColor: "white",
+                  margin: 0,
+                }}
+              >
+                <CheckBox text="remember me" value={loginFormState.check} />
+              </div>
+              <span className="forgot-password">forgot password</span>
             </div>
-            <span className="forgot-password">forgot password</span>
-          </div>
-          <Button text="login" handler={onSubmit} />
-        </form>
+            <Button
+              text={buttonText}
+              handler={onSubmit}
+              styles={{ backgroundColor: isLoading ? "#57457d" : "" }}
+            />
+          </form>
+        </div>
+        <footer>
+          <a href="#">Read Docs</a>
+        </footer>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+type Login = {
+  email: string;
+  password: string;
+};
+export const validateLogin = (data: Login): [boolean, string | null] => {
+  const { email, password } = data;
+  let error: string = "";
+  const re =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!email && !password) {
+    error = "Please fill in the fields ";
+  } else if (!re.test(email)) {
+    error = "Please enter a valid email address ";
+  } else if (!password) {
+    error = "Please enter a password ";
+  } else if (email !== "admin@mail.com" && password !== "admin") {
+    error = "invalid email or password ";
+  }
+  if (error) return [false, error];
+  else return [true, null];
+};
+
+export const waitFor = (seconds: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(null);
+    }, seconds * 1000);
+  });
+};
